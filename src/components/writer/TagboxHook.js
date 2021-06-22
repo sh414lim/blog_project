12c9fa9e6166978213f8d182df40bbcfdb1fa9be
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-06-22 19:31:03 +0900

    태그 에러발생

--- src/components/writer/TagboxHook.js
+++ src/components/writer/TagboxHook.js
@@ -16,20 +16,7 @@ h4{
 }
 
 `;
-const Tag=styled.div`
-margin-right: 0%.5rem;
-color:${palette.gray[6]};
-&:hover{
-    opacity:0.5 ;
-}
-`;
 
-const TagListBlock=styled.div`
-display: flex;
-flex-direction: column;
-margin-top: 0.5rem;
-
-`;
 
 const TagForm=styled.form`
 border-radius: 4px;
@@ -63,8 +50,81 @@ button{
 
 }
 `;
+const Tag=styled.div`
+margin-right: 0%.5rem;
+color:${palette.gray[6]};
+&:hover{
+    opacity:0.5 ;
+}
+`;
+
+const TagListBlock=styled.div`
+display: flex;
+flex-direction: column;
+margin-top: 0.5rem;
+
+`;
 
 //React.memo를 사용하여 tag 값이 바뀔 때만 리렌더링 되도록 처리
 const TagItem = React.memo(({tag,onRemove})=>(
     <Tag onClick={()=>onRemove(tag)}>#{tag}</Tag>
 ));
+
+//React.memo를 사용하여 tags 값이 바뀔 때만 리렌더링 되도록 처리
+const TagList = React.memo(({tags,onRemove})=>(
+    <TagListBlock>
+      {tags.map(tag =>(
+          <TagItem key={tag} tag={tag} onRemove={onRemove}/>
+      ))};
+    </TagListBlock>
+));
+
+const TagBox=()=>{
+    const[input,setInput]=useState('');
+    const[localTags,setLocalTags]=useState([]);
+
+    const insertTag=useCallback(
+        tag=>{
+            if(!tag)return; //공백이라면 추가하지 않음
+            if(localTags.includes(tag))return; //이미 존재한다면 추가하지 않음
+            setLocalTags([...localTags,tag]);
+        },
+        [localTags],
+    );
+
+    const onRemove=useCallback(
+        tag=>{
+            setLocalTags(localTags.filter(t=>t !== tag));
+        },
+    [localTags],
+    )
+
+    const onChange=useCallback(e=>{
+        setInput(e.target.value);
+    },[]);
+
+    const onSubmit=useCallback(
+        e=>{
+            e.preventDefault();
+            insertTag(input.trim()); //앞뒤 공백을 없앤 후 등록
+            setInput(''); //input 초기화
+        },
+        [input,insertTag],
+    );
+    return(
+        <TagBoxBlock>
+            <h4>태그</h4>
+            <TagForm onSubmit={onSubmit}>
+                <input
+                placeholder="태그를 입력하세요"
+                value={input}
+                onChange={onChange}
+                />
+                <button type="submit">추가</button>
+            </TagForm>
+            <TagList tag={localTags} onRemove={onRemove} />
+        </TagBoxBlock>
+    )
+}
+
+export default TagBox;
--- src/pages/WriterPages.js
+++ src/pages/WriterPages.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Responsive from "../components/conmon/Responsive";
 import Editor from "../components/writer/Editer";
-import TagBox from "../components/writer/TagBox"
+// import TagBox from "../components/writer/TagBox"
+import TagBox from "../components/writer/TagboxHook"
 
 const WritePage =()=>{
     return(
